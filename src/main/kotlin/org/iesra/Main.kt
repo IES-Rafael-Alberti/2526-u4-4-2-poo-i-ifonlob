71d4f46443c3332917d917c2f900da31061a19bc
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-01-17 17:01:12 +0100

    Main actualizado hasta implementación del primer ejercicio

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,16 +1,12 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val rectangulo1 = Rectangulo(15.0,25.0)
+    val rectangulo2 = Rectangulo(45.0,35.0)
+    val rectangulo3 = Rectangulo(30.0,20.0)
+    val listaRectangulos : List<Rectangulo> = listOf(rectangulo1,rectangulo2,rectangulo3)
+    for(rectangulo in listaRectangulos) {
+        println(rectangulo)
     }
+
 }
