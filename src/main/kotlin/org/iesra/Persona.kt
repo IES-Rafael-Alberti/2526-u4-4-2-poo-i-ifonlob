55ee3ba8866e806e82fd1a1af9c5486cd0696179
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-01-17 17:48:15 +0100

    Clase Persona actualizado según indicaciones del tercer ejercicio

--- src/main/kotlin/org/iesra/Persona.kt
+++ src/main/kotlin/org/iesra/Persona.kt
@@ -9,4 +9,32 @@ class Persona(var peso : Double,var altura : Double){
     override fun toString(): String {
         return "Persona con peso ${this.peso} kg, altura ${this.altura}m e IMC de ${String.format("%.2f", this.imc)}"
     }
+    fun saludar() : String{
+        return "Hola, me llamo ${this.nombre}."
+    }
+
+    fun alturaEncimaMedia() : Boolean{
+        return if(this.altura > 1.75){
+            true
+        }
+        else false
+    }
+    fun pesoEncimaMedia() : Boolean{
+        return if(this.peso > 70){
+            true
+        }
+        else false
+    }
+    private fun obtenerDescImc(): String {
+        val valor = imc
+        return when {
+            valor < 18.5 -> "peso insuficiente"
+            valor < 25.0 -> "peso saludable"
+            valor < 30.0 -> "sobrepeso"
+            else -> "obesidad"
+        }
+    }
+    fun obtenerDesc(): String {
+        return "La persona $nombre tiene un IMC de $imc, que corresponde a ${obtenerDescImc()}."
+    }
 }
